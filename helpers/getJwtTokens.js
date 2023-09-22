771d876887cf30796576d494a926750d6dc92a34
IMRAMAGRAWAL <imramagrawal@gmail.com>
2023-09-22 18:22:10 +0530

    first commit

---
+++ helpers/getJwtTokens.js
@@ -0,0 +1,15 @@
+// const jwt = require("jsonwebtoken")
+
+// const getJwtToken = (userId) => {
+//     return jwt.sign({userId: userId}), process.env.JWT_SECRET, {expiresIn: "1 day"}
+// }
+
+// module.exports = getJwtToken;
+
+const jwt = require("jsonwebtoken");
+
+const getJwtToken = (userId) => {
+    return jwt.sign({ userId: userId }, process.env.JWT_SECRET, { expiresIn: "1 day" });
+}
+
+module.exports = getJwtToken;
---
+++ index.js
@@ -0,0 +1,18 @@
+const cookieParser = require('cookie-parser')
+const express = require("express")
+const userRouter = require("./routes/userRoute")
+require("dotenv").config()
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+app.use(cookieParser())
+app.use("/api", userRouter)
+app.get("/", (req, res) =>{
+    res.send("hello")
+})
+
+app.listen(5000, () =>{
+    console.log("server is running on port 5000")
+})
